e0eba7bc60f2fb9d9e08274491cf1a6c4d84e71b
francis gonzales <fgonzales.private@icloud.com>
2024-10-03 11:19:49 +0800

    update header background color to white, font color to black

--- resources/views/user/includes/header.blade.php
+++ resources/views/user/includes/header.blade.php
@@ -1,14 +1,21 @@
 <header>
     <div class="fixed top-0 right-0 left-0 w-full font-Figtree z-[99]">
-        <nav
-            class="flex h-[56px] items-center justify-between bg-color-2C transition-all duration-500 ease-in-out dark:bg-[#141414]">
+
+
+        {{-- <nav
+            class="flex h-[56px] items-center justify-between bg-color-2C transition-all duration-500 ease-in-out dark:bg-[#141414]"> --}}
+            <nav class="flex h-[56px] items-center justify-between transition-all duration-500 ease-in-out bg-white">
             <div class="w-full h-[56px] flex items-center justify-between md:mx-5 gap-3">
 
                 <div class="flex items-center justify-center">
-                    <div class="w-[66px] h-[56px] mr-4 bg-[#464444] flex items-center justify-center md:hidden">
+                    <div class="w-[66px] h-[56px] mr-4 bg-[#464888] flex items-center justify-center md:hidden">
                         <svg class="collapse-icon pointer" width="26" height="26" viewBox="0 0 26 26" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
-                            <path d="M4 13H22M4 7H22M4 19H16" stroke="white" stroke-width="2" stroke-linecap="round"
+
+                            {{-- <path d="M4 13H22M4 7H22M4 19H16" stroke="white" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round"></path> --}}
+
+                            <path d="M4 13H22M4 7H22M4 19H16" stroke="black" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round"></path>
                         </svg>
                     </div>
@@ -34,12 +41,20 @@
                             
                             @if ($languages->isNotEmpty())
                             <div class="cursor-pointer mt-3 md:mt-0 relative">
-                                <a class="text-white text-16 cursor-pointer font-normal language-dropdown-click font-Figtree flex text-center gap-2 items-center justify-center">
+                                <a class=" text-16 cursor-pointer font-normal language-dropdown-click font-Figtree flex text-center gap-2 items-center justify-center">
                                     <img class="rounded-full cursor-pointer h-[18px] w-[18px] bg-white" src="{{ url("public/datta-able/fonts/flag/flags/4x3/" . getSVGFlag($flag) . ".svg") }}"
                                     alt="{{ __('Image') }}">
                                     <p> {{ ucFirst($flag) }} </p>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="13" viewBox="0 0 12 13" fill="none">
+
+
+                                    {{-- <svg xmlns="http://www.w3.org/2000/svg" width="12" height="13" viewBox="0 0 12 13" fill="none">
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M2.64645 4.64605C2.45118 4.84131 2.45118 5.15789 2.64645 5.35316L5.64645 8.35316C5.84171 8.54842 6.15829 8.54842 6.35355 8.35316L9.35355 5.35316C9.54882 5.15789 9.54882 4.84131 9.35355 4.64605C9.15829 4.45079 8.84171 4.45079 8.64645 4.64605L6 7.2925L3.35355 4.64605C3.15829 4.45079 2.84171 4.45079 2.64645 4.64605Z" fill="white"/>
+                                    </svg> --}}
+
+
+
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="13" viewBox="0 0 12 13" fill="none">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M2.64645 4.64605C2.45118 4.84131 2.45118 5.15789 2.64645 5.35316L5.64645 8.35316C5.84171 8.54842 6.15829 8.54842 6.35355 8.35316L9.35355 5.35316C9.54882 5.15789 9.54882 4.84131 9.35355 4.64605C9.15829 4.45079 8.84171 4.45079 8.64645 4.64605L6 7.2925L3.35355 4.64605C3.15829 4.45079 2.84171 4.45079 2.64645 4.64605Z" fill="black"/>
                                     </svg>
                                 </a>
                                 <div
@@ -49,7 +64,7 @@
                                             <a data-short-name="{{ $language->short_name }}" class="lang-change flex justify-start items-center gap-1.5 text-14 font-medium text-color-14 dark:text-white font-Figtree px-[15px] py-2 hover:bg-color-F6 dark:hover:bg-[#3A3A39]">
                                                 <img class="rounded-full cursor-pointer h-[18px] w-[18px] bg-white" src="{{ url("public/datta-able/fonts/flag/flags/4x3/" . getSVGFlag($language->short_name) . ".svg") }}"
                                                 alt="{{ __('Image') }}">
-                                                <p>{{ $language->name }}</p>
+                                                <p >{{ $language->name }}</p>
                                             </a>
                                         @endforeach
                                     </div>
@@ -65,11 +80,19 @@
                     <div>
                         <div class="relative">
                             <a class="user-header-dropdown-click flex justify-center items-center gap-2 cursor-pointer">
-                                <div class="text-white text-16 cursor-pointer font-normal font-Figtree flex text-center items-center justify-center rounded-full h-10 w-10 md:h-11 md:w-11 sign-in-button sign-in mr-2.5">
+
+
+                                {{-- <div class="text-white text-16 cursor-pointer font-normal font-Figtree flex text-center items-center justify-center rounded-full h-10 w-10 md:h-11 md:w-11 sign-in-button sign-in mr-2.5"> --}}
+
+                                <div class="text-16 cursor-pointer font-normal font-Figtree flex text-center items-center justify-center rounded-full h-10 w-10 md:h-11 md:w-11 sign-in-button sign-in mr-2.5">
                                 <img class="rounded-full h-9 w-9 md:h-10 md:w-10 cursor-pointer bg-white" src="{{ Auth::user()->fileUrl() }}"
                                 alt="Avatar of User">
                                 </div>
+
                                 <p class="text-white text-16 font-semibold font-Figtree line-clamp-single hidden md:block">
+
+
+                                <p class="text-16 font-semibold font-Figtree line-clamp-single hidden md:block">
                                     @php
                                     if(!empty($memberHeaderData['memberData']) && ($memberHeaderData['isMemberPackage'] === true || auth()->user()->hasCredit('word'))){
                                         echo session()->get('memberPackageData.packUserName');
