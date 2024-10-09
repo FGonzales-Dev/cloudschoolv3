fe790c38781f1de3eddf0f2965b1e27149bd874a
francis gonzales <fgonzales.private@icloud.com>
2024-10-09 18:51:52 +0800

    testing formatter

--- Modules/OpenAI/Resources/assets/js/customer/content.js
+++ Modules/OpenAI/Resources/assets/js/customer/content.js
@@ -1,67 +1,58 @@
-$('.AdavanceOption').on('click', function() {
-    if ($('#ProviderOptionDiv').attr('class') == 'hidden') {
-        hideProviderOptions()
-        $('.' + $('#provider option:selected').val() + '_div').removeClass('hidden');
-        $('#ProviderOptionDiv').removeClass('hidden');
-    } else {
-        $('#ProviderOptionDiv').addClass('hidden');
-    }
-});
-
-
-$('#provider').on('change', function() {
-    hideProviderOptions();
-    $('.' + $(this).val() + '_div').removeClass('hidden');
-});
-
-
-function hideProviderOptions() 
-{
-    $('.ProviderOptions').each(function() {
-        $(this).addClass('hidden')
+function hideProviderOptions() {
+    $(".ProviderOptions").each(function () {
+        $(this).addClass("hidden");
     });
 }
 
-$('#provider').on('change', function() {
+$(".AdavanceOption").on("click", function () {
+    if ($("#ProviderOptionDiv").attr("class") == "hidden") {
+        hideProviderOptions();
+        $("." + $("#provider option:selected").val() + "_div").removeClass("hidden");
+        $("#ProviderOptionDiv").removeClass("hidden");
+    } else {
+        $("#ProviderOptionDiv").addClass("hidden");
+    }
+});
+
+$("#provider").on("change", function () {
     hideProviderOptions();
-    $('.' + $(this).val() + '_div').removeClass('hidden');
+    $("." + $(this).val() + "_div").removeClass("hidden");
 });
 
 $(document).on("submit", "#openai-form", function (e) {
     let dataArray = $(this).serializeArray();
-
-    var providerObject = dataArray.find(function(element) {
-        return element.name === "provider";
+    var providerObject = dataArray.find(function (element) {
+        return "provider" === element.name;
     });
+
     function getValueByName(name) {
-        var item = dataArray.find(function(element) {
+        var item = dataArray.find(function (element) {
             return element.name === name;
         });
         return item ? item.value : null;
     }
 
-
-    
     var providerValue = providerObject ? providerObject.value : null;
- 
     let contentSlug = "";
-    var currentUrl = window.location.href.indexOf("content/edit") > -1;
-    if (currentUrl) {
-        (parts = window.location.href.split("/")),
-            (contentSlug = parts[parts.length - 1]);
+    var currentUrl;
+
+    if (window.location.href.indexOf("content/edit") > -1) {
+        parts = window.location.href.split("/");
+        contentSlug = parts[parts.length - 1];
     }
 
     e.preventDefault();
-    var gethtml = tinyMCE.activeEditor.getContent();
 
+    var gethtml = tinyMCE.activeEditor.getContent();
     var formData = {};
+
     $(".dynamic-input").each(function (column) {
         formData[this.name] = $(this).val();
     });
 
-    var url =  SITE_URL + "/" + 'user/generate';
-    var newUrl =  SITE_URL + "/" + 'user/process';
-    var questions = JSON.stringify(formData)
+    var url = SITE_URL + "/user/generate";
+    var newUrl = SITE_URL + "/user/process";
+    var questions = JSON.stringify(formData);
     var variant = getValueByName(`${providerValue}[variant]`);
     var language = getValueByName(`${providerValue}[language]`);
     var model = getValueByName(`${providerValue}[model]`);
@@ -70,8 +61,8 @@ $(document).on("submit", "#openai-form", function (e) {
         url: url,
         type: "POST",
         beforeSend: function (xhr) {
-            $(".loader").removeClass('hidden');
-            $('#magic-submit-button').prop('disabled', true);
+            $(".loader").removeClass("hidden");
+            $("#magic-submit-button").prop("disabled", true);
         },
         data: {
             questions: questions,
@@ -85,48 +76,61 @@ $(document).on("submit", "#openai-form", function (e) {
             provider: $("#provider").val(),
             previousContent: gethtml,
             contentSlug: contentSlug,
-            dataType: 'json',
+            dataType: "json",
             _token: CSRF_TOKEN
         },
-        success: function(response) {
-            if (response.status = 'success') {
-                var url = newUrl + "?questions=" + encodeURIComponent(questions) +"&provider=" + $("#provider").val() +"&useCase=" + $(".use-cases").val() +"&language=" + language +"&variant=" + variant +"&model=" + model +"&tone=" + $("#tone").val() + "&creativity_level=" + $("#creativity_level").val() + "&template_id=" + decodeURIComponent(response.data.templateId);
-                const eventSource = new EventSource(url, {withCredentials: true});
+        success: function (response) {
+            if (response.status = "success") {
+                var url = newUrl + "?questions=" + encodeURIComponent(questions) +
+                    "&provider=" + $("#provider").val() +
+                    "&useCase=" + $(".use-cases").val() +
+                    "&language=" + language +
+                    "&variant=" + variant +
+                    "&model=" + model +
+                    "&tone=" + $("#tone").val() +
+                    "&creativity_level=" + $("#creativity_level").val() +
+                    "&template_id=" + decodeURIComponent(response.data.templateId);
+
+                const eventSource = new EventSource(url, { withCredentials: true });
 
                 eventSource.onmessage = function (e) {
-                    if ( e.data == '[DONE]' ) {
+                    if (e.data == "[DONE]") {
                         eventSource.close();
                         toastMixin.fire({
-                            title: jsLang('Document generated successfully.'),
-                            icon: 'success'
+                            title: jsLang("Document generated successfully."),
+                            icon: "success"
                         });
-                        $(".loader").addClass('hidden');
+                        $(".loader").addClass("hidden");
                         $("#magic-submit-button").removeAttr("disabled");
-
-                    } else if (e.data == '[ERROR]') {
+                    } else if (e.data == "[ERROR]") {
                         eventSource.close();
-                        errorMessage(e.data, 'magic-submit-button');
+                        errorMessage(e.data, "magic-submit-button");
                     } else {
-                        let stream = e.data
-                        if ( stream && stream !== '[DONE]') {
+                        let stream = e.data;
+                        if (stream && stream !== "[DONE]") {
                             gethtml += stream;
-                            tinyMCE.activeEditor.setContent(gethtml, { format: "html" });
+                            console.log(gethtml);
+                            function makeBold(content) {
+                                return content.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
+                            }
+
+                            // Apply the bold formatting
+                            let formattedHtml = makeBold(gethtml);
+                            tinyMCE.activeEditor.setContent(formattedHtml, { format: "html" });
                         }
                     }
-
                 };
-                eventSource.addEventListener("error", (e) => {
+
+                eventSource.addEventListener("error", e => {
                     eventSource.close();
-                    errorMessage(e.data, 'magic-submit-button');
+                    errorMessage(e.data, "magic-submit-button");
                 });
             }
-
         },
         error: function (response) {
             var jsonData = JSON.parse(response.responseText);
             var message = jsonData.error ? jsonData.error : jsonData.message;
-            errorMessage(message, 'magic-submit-button');
+            errorMessage(message, "magic-submit-button");
         }
     });
-
 });
