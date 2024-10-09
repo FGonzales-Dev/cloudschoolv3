f13980819031f6e3cdf1c491cc05ac597eb5b1a2
francis gonzales <fgonzales.private@icloud.com>
2024-10-09 18:37:50 +0800

    Revert "Update TemplateResponse.php"
    This reverts commit 59f493f0123072023e20235ddcbb59de968d5c0e.

--- Modules/OpenAI/AiProviders/OpenAi/Responses/Template/TemplateResponse.php
+++ Modules/OpenAI/AiProviders/OpenAi/Responses/Template/TemplateResponse.php
@@ -67,9 +67,6 @@ class TemplateResponse implements TemplateResponseContract
     public function content(): string
     {
         $content = $this->response['choices'][0]['message']['content'];
-        $content = preg_replace('/\*\*(.*?)\*\*/', '<strong>$1</strong>', $content);
-    
-        // Set and return the modified content
         return $this->content = $content;
     }
 
