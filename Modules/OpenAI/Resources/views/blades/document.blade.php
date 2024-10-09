1b27579fea4a06335dfc0fbed3ea97fde8f453e4
francis gonzales <fgonzales.private@icloud.com>
2024-10-09 14:31:31 +0800

    test

--- Modules/OpenAI/Resources/views/blades/document.blade.php
+++ Modules/OpenAI/Resources/views/blades/document.blade.php
@@ -115,6 +115,8 @@
 
                 </div>
                 <textarea id="basic-example" class="hidden">
+                    Testing
+                    {!! nl2br(preg_replace('/\*\*(.*?)\*\*/', '<strong>$1</strong>', 'Test1 **Slide 1:** Test1')) !!}
                     {!! !empty($useCase->content)
                         ? nl2br(preg_replace('/\*\*(.*?)\*\*/', '<strong>$1</strong>', htmlspecialchars($useCase->content)))
                         : '' !!}
