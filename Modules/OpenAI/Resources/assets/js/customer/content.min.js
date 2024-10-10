e21bad6cabde88420f88b5368e2672b646a51e42
francis gonzales <fgonzales.private@icloud.com>
2024-10-10 16:15:09 +0800

    revert back to original code

--- Modules/OpenAI/Resources/assets/js/customer/content.min.js
+++ Modules/OpenAI/Resources/assets/js/customer/content.min.js
@@ -1,152 +1,130 @@
-function loadScript(url, callback) {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-
-    script.onload = function () {
-        if (callback) callback();
-    };
-
-    document.head.appendChild(script);
+function hideProviderOptions() {
+    $(".ProviderOptions").each(function () {
+        $(this).addClass("hidden");
+    });
 }
 
-loadScript('https://cdnjs.cloudflare.com/ajax/libs/marked/2.1.3/marked.min.js', function () {
-    console.log("Loaded marked.min.js successfully!");
-
-
-    if (typeof marked === 'undefined') {
-        console.error("Marked is not defined!");
-    } else {
-        console.log("Marked is available for use.");
-    }
-    function hideProviderOptions() {
-        $(".ProviderOptions").each(function () {
-            $(this).addClass("hidden");
-        });
-    }
-
-    $(".AdavanceOption").on("click", function () {
-        if ($("#ProviderOptionDiv").attr("class") == "hidden") {
-            hideProviderOptions();
-            $("." + $("#provider option:selected").val() + "_div").removeClass("hidden");
-            $("#ProviderOptionDiv").removeClass("hidden");
-        } else {
-            $("#ProviderOptionDiv").addClass("hidden");
-        }
-    });
-
-    $("#provider").on("change", function () {
+$(".AdavanceOption").on("click", function () {
+    if ($("#ProviderOptionDiv").attr("class") == "hidden") {
         hideProviderOptions();
-        $("." + $(this).val() + "_div").removeClass("hidden");
+        $("." + $("#provider option:selected").val() + "_div").removeClass("hidden");
+        $("#ProviderOptionDiv").removeClass("hidden");
+    } else {
+        $("#ProviderOptionDiv").addClass("hidden");
+    }
+});
+
+$("#provider").on("change", function () {
+    hideProviderOptions();
+    $("." + $(this).val() + "_div").removeClass("hidden");
+});
+
+$(document).on("submit", "#openai-form", function (e) {
+    let dataArray = $(this).serializeArray();
+    var providerObject = dataArray.find(function (element) {
+        return "provider" === element.name;
     });
 
-    $(document).on("submit", "#openai-form", function (e) {
-        let dataArray = $(this).serializeArray();
-        var providerObject = dataArray.find(function (element) {
-            return "provider" === element.name;
+    function getValueByName(name) {
+        var item = dataArray.find(function (element) {
+            return element.name === name;
         });
+        return item ? item.value : null;
+    }
 
-        function getValueByName(name) {
-            var item = dataArray.find(function (element) {
-                return element.name === name;
-            });
-            return item ? item.value : null;
-        }
+    var providerValue = providerObject ? providerObject.value : null;
+    let contentSlug = "";
+    var currentUrl;
 
-        var providerValue = providerObject ? providerObject.value : null;
-        let contentSlug = "";
-        var currentUrl;
+    if (window.location.href.indexOf("content/edit") > -1) {
+        parts = window.location.href.split("/");
+        contentSlug = parts[parts.length - 1];
+    }
 
-        if (window.location.href.indexOf("content/edit") > -1) {
-            parts = window.location.href.split("/");
-            contentSlug = parts[parts.length - 1];
-        }
+    e.preventDefault();
 
-        e.preventDefault();
+    var gethtml = tinyMCE.activeEditor.getContent();
+    var formData = {};
 
-        var gethtml = tinyMCE.activeEditor.getContent();
-        var formData = {};
+    $(".dynamic-input").each(function (column) {
+        formData[this.name] = $(this).val();
+    });
 
-        $(".dynamic-input").each(function (column) {
-            formData[this.name] = $(this).val();
-        });
+    var url = SITE_URL + "/user/generate";
+    var newUrl = SITE_URL + "/user/process";
+    var questions = JSON.stringify(formData);
+    var variant = getValueByName(`${providerValue}[variant]`);
+    var language = getValueByName(`${providerValue}[language]`);
+    var model = getValueByName(`${providerValue}[model]`);
 
-        var url = SITE_URL + "/user/generate";
-        var newUrl = SITE_URL + "/user/process";
-        var questions = JSON.stringify(formData);
-        var variant = getValueByName(`${providerValue}[variant]`);
-        var language = getValueByName(`${providerValue}[language]`);
-        var model = getValueByName(`${providerValue}[model]`);
+    $.ajax({
+        url: url,
+        type: "POST",
+        beforeSend: function (xhr) {
+            $(".loader").removeClass("hidden");
+            $("#magic-submit-button").prop("disabled", true);
+        },
+        data: {
+            questions: questions,
+            prompt: filterXSS($("#promt").val()),
+            useCase: $(".use-cases").val(),
+            language: language,
+            variant: variant,
+            model: model,
+            tone: getValueByName(`${providerValue}[tone]`),
+            creativity_level: getValueByName(`${providerValue}[creativity_level]`),
+            provider: $("#provider").val(),
+            previousContent: gethtml,
+            contentSlug: contentSlug,
+            dataType: "json",
+            _token: CSRF_TOKEN
+        },
+        success: function (response) {
+            if (response.status = "success") {
+                var url = newUrl + "?questions=" + encodeURIComponent(questions) +
+                    "&provider=" + $("#provider").val() +
+                    "&useCase=" + $(".use-cases").val() +
+                    "&language=" + language +
+                    "&variant=" + variant +
+                    "&model=" + model +
+                    "&tone=" + $("#tone").val() +
+                    "&creativity_level=" + $("#creativity_level").val() +
+                    "&template_id=" + decodeURIComponent(response.data.templateId);
 
-        $.ajax({
-            url: url,
-            type: "POST",
-            beforeSend: function (xhr) {
-                $(".loader").removeClass("hidden");
-                $("#magic-submit-button").prop("disabled", true);
-            },
-            data: {
-                questions: questions,
-                prompt: filterXSS($("#promt").val()),
-                useCase: $(".use-cases").val(),
-                language: language,
-                variant: variant,
-                model: model,
-                tone: getValueByName(`${providerValue}[tone]`),
-                creativity_level: getValueByName(`${providerValue}[creativity_level]`),
-                provider: $("#provider").val(),
-                previousContent: gethtml,
-                contentSlug: contentSlug,
-                dataType: "json",
-                _token: CSRF_TOKEN
-            },
-            success: function (response) {
-                if (response.status = "success") {
-                    var url = newUrl + "?questions=" + encodeURIComponent(questions) +
-                        "&provider=" + $("#provider").val() +
-                        "&useCase=" + $(".use-cases").val() +
-                        "&language=" + language +
-                        "&variant=" + variant +
-                        "&model=" + model +
-                        "&tone=" + $("#tone").val() +
-                        "&creativity_level=" + $("#creativity_level").val() +
-                        "&template_id=" + decodeURIComponent(response.data.templateId);
+                const eventSource = new EventSource(url, { withCredentials: true });
 
-                    const eventSource = new EventSource(url, { withCredentials: true });
-
-                    eventSource.onmessage = function (e) {
-                        if (e.data == "[DONE]") {
-                            eventSource.close();
-                            toastMixin.fire({
-                                title: jsLang("Document generated successfully."),
-                                icon: "success"
-                            });
-                            $(".loader").addClass("hidden");
-                            $("#magic-submit-button").removeAttr("disabled");
-                        } else if (e.data == "[ERROR]") {
-                            eventSource.close();
-                            errorMessage(e.data, "magic-submit-button");
-                        } else {
-                            let stream = e.data;
-                            if (stream && stream !== "[DONE]") {
-                                console.log(stream);
-                                gethtml += stream;
-                                tinyMCE.activeEditor.setContent(gethtml, { format: "html" });
-                            }
-                        }
-                    };
-
-                    eventSource.addEventListener("error", e => {
+                eventSource.onmessage = function (e) {
+                    if (e.data == "[DONE]") {
+                        eventSource.close();
+                        toastMixin.fire({
+                            title: jsLang("Document generated successfully."),
+                            icon: "success"
+                        });
+                        $(".loader").addClass("hidden");
+                        $("#magic-submit-button").removeAttr("disabled");
+                    } else if (e.data == "[ERROR]") {
                         eventSource.close();
                         errorMessage(e.data, "magic-submit-button");
-                    });
-                }
-            },
-            error: function (response) {
-                var jsonData = JSON.parse(response.responseText);
-                var message = jsonData.error ? jsonData.error : jsonData.message;
-                errorMessage(message, "magic-submit-button");
+                    } else {
+                        let stream = e.data;
+                        if (stream && stream !== "[DONE]") {
+                            console.log(stream)
+                            gethtml += stream;
+                            tinyMCE.activeEditor.setContent(gethtml, { format: "html" });
+                        }
+                    }
+                };
+
+                eventSource.addEventListener("error", e => {
+                    eventSource.close();
+                    errorMessage(e.data, "magic-submit-button");
+                });
             }
-        });
+        },
+        error: function (response) {
+            var jsonData = JSON.parse(response.responseText);
+            var message = jsonData.error ? jsonData.error : jsonData.message;
+            errorMessage(message, "magic-submit-button");
+        }
     });
 });
