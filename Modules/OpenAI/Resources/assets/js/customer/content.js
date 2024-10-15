c299718d460726b7ea419a0098d4fa87969ebc9a
francis gonzales <fgonzales.private@icloud.com>
2024-10-15 15:47:27 +0800

    testing formatting

--- Modules/OpenAI/Resources/assets/js/customer/content.js
+++ Modules/OpenAI/Resources/assets/js/customer/content.js
@@ -6,14 +6,17 @@ script.onload = function () {
         console.log('Marked.js loaded successfully');
 
 
-        var markdownText = `**Bold text** and *italic text*
-        # Header1
-         and 
-          ## Header 2
-           and 
-           ### Header 3 
-           and 
-           #### Header 4`;
+        var markdownText = `
+        **Bold text** and *italic text*
+        
+        # Header 1
+        
+        ## Header 2
+        
+        ### Header 3
+        
+        #### Header 4
+                `;
         var htmlContent = window.marked(markdownText);
         console.log(htmlContent);
     } else {
